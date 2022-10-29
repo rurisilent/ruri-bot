45e577456562a32d019501756a4714cf3ef7db2b
rurisilent <rurisilent@gmail.com>
2022-10-29 23:01:48 +0800

    Adds logger

---
+++ RuriBot.Library/Log/IRRBotLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuriBot.Library.Log
+{
+    public interface IRRBotLogger
+    {
+        void Log(string content);
+    }
+}
